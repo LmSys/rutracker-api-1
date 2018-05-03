1511d9c61478358dd5865d4b8261b78079f686e7
pshenichnyj <pavel.pshenichnyj@auroraglobal.com>
2018-05-03 15:46:14 +0300

    added get torrent by id, and category id for search

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
-const Parser = require("./lib/parser");
+const Parser       = require("./lib/parser");
 const PageProvider = require("./lib/page-provider");
 
 class RutrackerApi {
-  constructor() {
-    this.parser = new Parser();
-    this.pageProvider = new PageProvider();
-  }
+    constructor() {
+        this.parser       = new Parser();
+        this.pageProvider = new PageProvider();
+    }
 
-  login({ username, password }) {
-    return this.pageProvider.login(username, password);
-  }
+    login({username, password}) {
+        return this.pageProvider.login(username, password);
+    }
 
-  search({ query, sort, order }) {
-    return this.pageProvider
-      .search({ query, sort, order })
-      .then(html => this.parser.parseSearch(html));
-  }
+    search({query, sort, order}) {
+        return this.pageProvider
+            .search({query, sort, order})
+            .then(html => this.parser.parseSearch(html));
+    }
 
-  download(id) {
-    return this.pageProvider.torrentFile(id);
-  }
+    download(id) {
+        return this.pageProvider.torrentFile(id);
+    }
 
-  getMagnetLink(id) {
-    return this.pageProvider
-      .thread(id)
-      .then(html => this.parser.parseMagnetLink(html));
-  }
+    getMagnetLink(id) {
+        return this.pageProvider
+            .thread(id)
+            .then(html => this.parser.parseMagnetLink(html));
+    }
+
+    get(id) {
+        return this.pageProvider
+            .thread(id)
+            .then(html => this.parser.parsePage(html));
+    }
 }
 
 module.exports = RutrackerApi;
